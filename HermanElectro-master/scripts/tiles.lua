26653ccbc12d4ccea271c53f95420f16899367c1
themost1 <benjaminmost@gmail.com>
2016-07-10 21:30:53 -0700

    New glass tiles

--- HermanElectro-master/scripts/tiles.lua
+++ HermanElectro-master/scripts/tiles.lua
@@ -1196,6 +1196,8 @@ P.lamp = P.tile:new{name = "lamp", sprite = love.graphics.newImage('Graphics/lam
 
 P.conductiveGlass = P.glassWall:new{name = "conductiveGlass", sprite = love.graphics.newImage('Graphics3D/conductiveglass.png'), poweredSprite = love.graphics.newImage('Graphics3D/conductiveglass.png'), canBePowered = true, dirAccept = {1,1,1,1}, dirSend = {1,1,1,1}}
 
+P.reinforcedConductiveGlass = P.reinforcedGlass:new{name = "reinforcedConductiveGlass", sprite = love.graphics.newImage('Graphics3D/reinforcedconductiveglass.png'), poweredSprite = love.graphics.newImage('Graphics3D/reinforcedconductiveglass.png'), canBePowered = true, dirAccept = {1,1,1,1}, dirSend = {1,1,1,1}}
+
 tiles[1] = P.invisibleTile
 tiles[2] = P.conductiveTile
 tiles[3] = P.powerSupply
@@ -1275,5 +1277,6 @@ tiles[76] = P.batteringRamTile
 tiles[77] = P.lamp
 tiles[78] = P.glue
 tiles[79] = P.conductiveGlass
+tiles[80] = P.reinforcedConductiveGlass
 
 return tiles
