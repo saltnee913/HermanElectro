85187157eac312a57e2ddef46dc69fee3ebac217
themost1 <benjaminmost@gmail.com>
2016-10-10 16:29:10 -0400

    Fixed Giovanni stay button bug

--- HermanElectro-master/scripts/characters.lua
+++ HermanElectro-master/scripts/characters.lua
@@ -268,6 +268,7 @@ function P.giovanni:onKeyPressed(key)
 			player.tileY = self.shiftPos.y
 			self.shiftPos = {x = -1, y = -1}
 			log("Returned to clone!")
+			updateGameState(false)
 		end
 	end
 end
