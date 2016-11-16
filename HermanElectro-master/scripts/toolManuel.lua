f89c9922e0feb8e1c7a48ec93d3a19592decce2c
ile123ile <bbravencofounder@gmail.com>
2016-11-15 16:31:59 -0800

    Added side tabs for tool manuel

--- HermanElectro-master/scripts/toolManuel.lua
+++ HermanElectro-master/scripts/toolManuel.lua
@@ -5,22 +5,47 @@ P.opened = false
 
 P.mainScreen = love.graphics.newImage('NewGraphics/MenuTemplateA.png')
 P.manuelTint = love.graphics.newImage('NewGraphics/manuelTint.png')
-P.screens = {love.graphics.newImage('NewGraphics/SawTutA.png'), love.graphics.newImage('NewGraphics/LadderTutA.png')}
+P.tabBG = love.graphics.newImage('NewGraphics/manuelTabBG.png')
+P.tabBGSelected = love.graphics.newImage('NewGraphics/manuelTabSelectedBG.png')
+
+P.sawScreens = {love.graphics.newImage('NewGraphics/SawTutA.png')}
+P.ladderScreens = {love.graphics.newImage('NewGraphics/LadderTutA.png')}
+P.wireCutterScreens = {}
+P.waterScreens = {}
+P.spongeScreens = {}
+P.brickScreens = {}
+P.gunScreens = {}
+P.screens = {P.sawScreens, P.ladderScreens}--, P.wireCutterScreens, P.waterScreens, P.spongeScreens, P.brickScreens, P.gunScreens}
+
 P.currentScreen = 1
+P.screenLevel = 1
 
 function P.draw()
-	local screen = P.screens[P.currentScreen]
+	local screen = P.screens[P.currentScreen][P.screenLevel]
 	local scale = {x = width/P.mainScreen:getWidth(), y = height/P.mainScreen:getHeight()}
 	love.graphics.draw(P.mainScreen, 0, 0, 0, scale.x, scale.y)
 	love.graphics.draw(screen, 22*scale.x, 64*scale.y, 0, scale.x, scale.y)
 	love.graphics.draw(P.manuelTint, (40*P.currentScreen-18)*scale.x, (5)*scale.y, 0, scale.x, scale.y)
+	for i = 1, #(P.screens[P.currentScreen]) do
+		if P.screenLevel == i then
+			love.graphics.draw(P.tabBGSelected, 4*scale.x, (40+26*i)*scale.y, 0, scale.x, scale.y)
+		else
+			love.graphics.draw(P.tabBG, 4*scale.x, (40+26*i)*scale.y, 0, scale.x, scale.y)
+		end
+	end
 end
 
 function P.keypressed(key, unicode)
 	if key == 'right' or key == 'd' then
 		P.currentScreen = P.currentScreen + 1
+		P.screenLevel = 1
 	elseif key == 'left' or key == 'a' then
 		P.currentScreen = P.currentScreen - 1
+		P.screenLevel = 1
+	elseif key == 'down' or key == 's' then
+		P.screenLevel = P.screenLevel + 1
+	elseif key == 'up' or key == 'w' then
+		P.screenLevel = P.screenLevel - 1
 	elseif key == 'escape' then
 		P.close()
 	end
@@ -28,6 +53,11 @@ function P.keypressed(key, unicode)
 		P.currentScreen = #P.screens
 	elseif P.currentScreen > #P.screens then
 		P.currentScreen = 1
+	end
+	if P.screenLevel < 1 then
+		P.screenLevel = #(P.screens[P.currentScreen])
+	elseif P.screenLevel > #(P.screens[P.currentScreen]) then
+		P.screenLevel = 1
 	end
 end
 
