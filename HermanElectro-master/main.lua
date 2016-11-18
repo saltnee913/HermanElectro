9ecbca2a374f3f6d5dbca3cbed539e06d0a4ed0e
themost1 <benjaminmost@gmail.com>
2016-11-18 18:06:08 -0500

    Fixed bomb-revive interaction

--- HermanElectro-master/main.lua
+++ HermanElectro-master/main.lua
@@ -475,6 +475,35 @@ function kill()
 		unlocks.unlockUnlockableRef(unlocks.portalUnlock)
 	end
 	player.dead = true
+	for i = 1, #tools do
+		if not tools[i]:checkDeath() then
+			player.dead = false
+			--[[for i = 1, tools.numNormalTools do
+				tools[i].numHeld = 0
+			end]]
+			for i = 1, roomHeight do
+				for j = 1, roomLength do
+					if room[i][j]~=nil then
+						if not room[i][j]:instanceof(tiles.endTile) and not room[i][j]:instanceof(tiles.tunnel) then
+							room[i][j]=tiles.invisibleTile:new()
+						end
+					end
+				end
+			end
+	
+			for i = 1, #animals do
+				animals[i]:kill()
+			end
+			for j = 1, #pushables do
+				pushables[j]:destroy()
+			end
+			updateGameState(false)
+			log("Revived!")
+			stats.losses[player.character.name] = stats.losses[player.character.name]-1
+			onToolUse(i)
+			break
+		end
+	end
 	unlockedChars = characters.getUnlockedCharacters()
 	stats.losses[player.character.name] = stats.losses[player.character.name]+1
 	stats.writeStats()
@@ -2463,37 +2492,6 @@ function checkDeath()
 	for i = 1, #animals do
 		if animals[i]:willKillPlayer(player) and not player.safeFromAnimals then
 			kill()
-		end
-	end
-	if player.dead then
-		for i = 1, #tools do
-			if not tools[i]:checkDeath() then
-				player.dead = false
-				--[[for i = 1, tools.numNormalTools do
-					tools[i].numHeld = 0
-				end]]
-				for i = 1, roomHeight do
-					for j = 1, roomLength do
-						if room[i][j]~=nil then
-							if not room[i][j]:instanceof(tiles.endTile) and not room[i][j]:instanceof(tiles.tunnel) then
-								room[i][j]=tiles.invisibleTile:new()
-							end
-						end
-					end
-				end
-		
-				for i = 1, #animals do
-					animals[i]:kill()
-				end
-				for j = 1, #pushables do
-					pushables[j]:destroy()
-				end
-				updateGameState(false)
-				log("Revived!")
-				stats.losses[player.character.name] = stats.losses[player.character.name]-1
-				onToolUse(i)
-				break
-			end
 		end
 	end
 end
