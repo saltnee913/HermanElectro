5d64375e07f7bfe35ddf146b3622dbed74f5c5ec
themost1 <benjaminmost@gmail.com>
2016-11-17 19:36:21 -0500

    Monk revised

--- HermanElectro-master/scripts/characters.lua
+++ HermanElectro-master/scripts/characters.lua
@@ -390,33 +390,34 @@ end
 
 P.monk = P.character:new{name = "Monte", description = "The Blind Monk", sprite = love.graphics.newImage('Graphics/monk.png')}
 function P.monk:onCharLoad()
-	tools.giveToolsByReference({tools.lamp})
+	for i = 1, 3 do
+		self.tint[i] = 0
+	end
+	myShader:send("tint_r", self.tint[1])
+    myShader:send("tint_g", self.tint[2])
+    myShader:send("tint_b", self.tint[3])
+	tools.giveToolsByReference({tools.lamp, tools.lamp, tools.lamp})
 end
 function P.monk:onFloorEnter()
-	tools.giveToolsByReference({tools.lamp})
+	for i = 1, 3 do
+		self.tint[i] = 0
+	end
+	myShader:send("tint_r", self.tint[1])
+    myShader:send("tint_g", self.tint[2])
+    myShader:send("tint_b", self.tint[3])
+	tools.giveToolsByReference({tools.lamp, tools.lamp, tools.lamp})
 end
-function P.monk:onPostUpdateLight()
-	local allUnlit = false
-	local breakLoop = false
-	for i = 1, roomHeight do
-		for j = 1, roomLength do
-			if room[i][j]~=nil and room[i][j].lit then
-				allUnlit = true
-				breakLoop = true
-				break
-			end
+function P.monk:postMove()
+	for i = 1, 3 do
+		if self.tint[i]==0 then
+			self.tint[i]=0.01
 		end
-		if breakLoop then break end
+		self.tint[i] = self.tint[i]+(1-self.tint[i])/100
+		if self.tint[i]>0.45 then self.tint[i] = 0.45 end
 	end
-
-	for i = 1, roomHeight do
-		for j = 1, roomLength do
-			if litTiles[i][j]==1 and (math.abs(j-player.tileX)+math.abs(i-player.tileY)>2) then
-				litTiles[i][j]=0
-			elseif allUnlit then litTiles[i][j]=0
-			end
-		end
-	end
+    myShader:send("tint_r", self.tint[1])
+    myShader:send("tint_g", self.tint[2])
+    myShader:send("tint_b", self.tint[3])
 end
 
 P.random2 = P.character:new{name = "Random2", allowedCharacters = {1,2,6,8,9,11,12,14}, description = "**RanDOm**", sprite = love.graphics.newImage('Graphics/random.png'), disabled = true}
