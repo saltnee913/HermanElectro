6d173b132d1602c2b3521f421e0489933c7f1432
themost1 <benjaminmost@gmail.com>
2016-12-27 02:10:57 -0500

    Fixed floor ordering bug

--- HermanElectro-master/main.lua
+++ HermanElectro-master/main.lua
@@ -552,8 +552,8 @@ function loadNextLevel(dontChangeTime)
 		if floorIndex > #map.floorOrder then
 			floorIndex = 1
 		end
-		floorIndex = floorIndex + 1
 		loadLevel(map.floorOrder[floorIndex])
+		floorIndex = floorIndex+1
 		playMusic(floorIndex)
 	end
 	--hack to make it not happen on the first floor
