88c7c1b66f42759a72446f8f7575ccec64c6b979
ile123ile <bbravencofounder@gmail.com>
2017-01-01 17:37:02 -0800

    Removed the mfing wings

--- HermanElectro-master/scripts/characters.lua
+++ HermanElectro-master/scripts/characters.lua
@@ -144,7 +144,6 @@ function P.erik:onCharLoad()
 	gameTime.roomTime = 15
 	gameTime.levelTime = 0
 	gameTime.goesDownInCompleted = true
-	tools.giveToolsByReference({tools.wings,tools.wings,tools.wings,tools.wings,tools.wings,tools.wings})
 	--map.floorOrder = {'RoomData/floor1_erik.json', 'RoomData/floor2_erik.json', 'RoomData/floor3_erik.json', 'RoomData/floor6.json'}
 end
 function P.erik:onFailedMove(key)
