afc84589bf58e7e0d01b9411791a992bda1c5815
eknighton <eknighton@me.com>
2017-01-09 15:23:57 -0800

    Fixed Tunneler deep issue

--- HermanElectro-master/scripts/tools.lua
+++ HermanElectro-master/scripts/tools.lua
@@ -3136,7 +3136,7 @@ P.numNormalTools = 7
 ]]
 
 function P.resetTools()
-	P[1] = P.tunneler
+	P[1] = P.saw
 	P[2] = P.ladder
 	P[3] = P.wireCutters
 	P[4] = P.waterBottle
@@ -3270,7 +3270,7 @@ P:addTool(P.supertoolReroller)
 
 
 
-:addTool(P.tunneler)
+P:addTool(P.tunneler)
 
 P.resetTools()
 
