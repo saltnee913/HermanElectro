27628baee63c3d5913015e1d3f6f03db42f2aacd
ile123ile <bbravencofounder@gmail.com>
2017-04-04 21:26:11 -0700

    Made green in tool manuel look good

--- HermanElectro-master/scripts/toolManuel.lua
+++ HermanElectro-master/scripts/toolManuel.lua
@@ -13,76 +13,89 @@ local function drawHere(image, x, y, rotation)
 	if rotation == nil then rotation = 0 end
 	love.graphics.draw(util.getImage(image), x*scale.x, y*scale.y, rotation, scale.x, scale.y)
 end
-local function drawAtTile(image, tileX, tileY, x, y, rotation)
-	drawHere(image, x + tileX*tileWidth, y + tileY*tileHeight, rotation)
+local function drawAtTile(image, tileX, tileY, x, y, yOffset, rotation)
+	if yOffset == nil then yOffset = 0 end
+	drawHere(image, x + tileX*tileWidth, y + tileY*tileHeight + yOffset, rotation)
+end
+local function drawGreen(tileX, tileY, x, y, yOffset)
+	if yOffset == nil then yOffset = 0 end
+	love.graphics.draw(util.getImage(green), (x+tileWidth*tileX)*scale.x, (y+tileHeight*tileY+yOffset)*scale.y, rotation, scale.x, scale.y*(16-yOffset)/16)
 end
 
 P.sawScreens = {
-function(x, y, scale)
-	drawAtTile(tiles.wall.sprite, 1, 0, x, y)
-	drawAtTile(player.character.sprite, 0, 0, x, y)
-	drawAtTile(green, 1, 0, x, y)
+	function(x, y, scale)
+		drawAtTile(tiles.wall.sprite, 1, 0, x, y, tiles.wall.yOffset)
+		drawAtTile(player.character.sprite, 0, 0, x, y)
+		drawGreen(1, 0, x, y, tiles.wall.yOffset)
 
-	drawAtTile(tiles.wall.destroyedSprite, 8, 0, x, y)
-	drawAtTile(player.character.sprite, 7, 0, x, y)
-end,
-function(x, y, scale)
-	drawAtTile(tiles.metalWall.sprite, 1, 0, x, y)
-	drawAtTile(player.character.sprite, 0, 0, x, y)
-	drawAtTile(green, 1, 0, x, y)
+		drawAtTile(tiles.wall.destroyedSprite, 8, 0, x, y)
+		drawAtTile(player.character.sprite, 7, 0, x, y)
+	end,
+	function(x, y, scale)
+		drawAtTile(tiles.metalWall.sprite, 1, 0, x, y, tiles.metalWall.yOffset)
+		drawAtTile(player.character.sprite, 0, 0, x, y)
+		drawGreen(1, 0, x, y, tiles.metalWall.yOffset)
 
-	drawAtTile(tiles.metalWall.destroyedSprite, 8, 0, x, y)
-	drawAtTile(player.character.sprite, 7, 0, x, y)
+		drawAtTile(tiles.metalWall.destroyedSprite, 8, 0, x, y)
+		drawAtTile(player.character.sprite, 7, 0, x, y)
 
 
-	drawAtTile(tiles.powerSupply.sprite, 1, 4, x, y)
-	drawAtTile(tiles.metalWall.poweredSprite, 1, 5, x, y)
-	drawAtTile(tiles.wire.poweredSprite, 1, 6, x, y)
-	drawAtTile(player.character.sprite, 0, 5, x, y)
-	drawAtTile(green, 1, 5, x, y)
+		drawAtTile(tiles.powerSupply.sprite, 1, 4, x, y)
+		drawAtTile(tiles.metalWall.poweredSprite, 1, 5, x, y, tiles.metalWall.yOffset)
+		drawAtTile(tiles.verticalWire.poweredSprite, 1, 6, x, y)
+		drawAtTile(player.character.sprite, 0, 5, x, y)
+		drawGreen(1, 5, x, y, tiles.metalWall.yOffset)
 
-	drawAtTile(tiles.powerSupply.sprite, 8, 4, x, y)
-	drawAtTile(tiles.metalWall.destroyedSprite, 8, 5, x, y)
-	drawAtTile(tiles.wire.sprite, 8, 6, x, y)
-	drawAtTile(player.character.sprite, 7, 5, x, y)
-end
+		drawAtTile(tiles.powerSupply.sprite, 8, 4, x, y)
+		drawAtTile(tiles.metalWall.destroyedSprite, 8, 5, x, y)
+		drawAtTile(tiles.verticalWire.sprite, 8, 6, x, y)
+		drawAtTile(player.character.sprite, 7, 5, x, y)
+	end,
+	function(x, y, scale)
+		drawAtTile(pushableList.box.sprite, 1, 0, x, y)
+		drawAtTile(player.character.sprite, 0, 0, x, y)
+		drawGreen(1, 0, x, y)
+
+		drawAtTile(player.character.sprite, 7, 0, x, y)
+	end
 }
 
 P.ladderScreens = {
-function(x, y, scale)
-	drawAtTile(tiles.pit.sprite, 1, 0, x, y)
-	drawAtTile(player.character.sprite, 0, 0, x, y)
-	drawAtTile(green, 1, 0, x, y)
+	function(x, y, scale)
+		drawAtTile(tiles.pit.sprite, 1, 0, x, y)
+		drawAtTile(player.character.sprite, 0, 0, x, y)
+		drawGreen(1, 0, x, y)
 
-	drawAtTile(tiles.pit.destroyedSprite, 8, 0, x, y)
-	drawAtTile(player.character.sprite, 7, 0, x, y)
+		drawAtTile(tiles.pit.destroyedSprite, 8, 0, x, y)
+		drawAtTile(player.character.sprite, 7, 0, x, y)
 
 
-	drawAtTile(tiles.poweredFloor.sprite, 1, 4, x, y)
-	drawAtTile(player.character.sprite, 0, 4, x, y)
-	drawAtTile(green, 1, 4, x, y)
+		drawAtTile(tiles.poweredFloor.sprite, 1, 4, x, y)
+		drawAtTile(player.character.sprite, 0, 4, x, y)
+		drawGreen(1, 4, x, y)
 
-	drawAtTile(tiles.poweredFloor.destroyedSprite, 8, 4, x, y)
-	drawAtTile(player.character.sprite, 7, 4, x, y)
-end,
-function(x, y, scale)
-	drawAtTile(green, 2, 0, x, y)
-	drawAtTile(animalList.pitbull.sprite, 3, 0, x, y)
-	drawAtTile(player.character.sprite, 1, 0, x, y)
+		drawAtTile(tiles.poweredFloor.destroyedSprite, 8, 4, x, y)
+		drawAtTile(player.character.sprite, 7, 4, x, y)
+	end,
+	function(x, y, scale)
+		drawGreen(2, 0, x, y)
+		drawAtTile(animalList.pitbull.sprite, 3, 0, x, y)
+		drawAtTile(player.character.sprite, 1, 0, x, y)
 
-	drawAtTile(tiles.ladder.sprite, 9, 0, x, y)
-	drawAtTile(animalList.pitbull.sprite, 10, 0, x, y)
-	drawAtTile(player.character.sprite, 8, 0, x, y)
+		drawAtTile(tiles.ladder.sprite, 9, 0, x, y)
+		drawAtTile(animalList.pitbull.sprite, 10, 0, x, y)
+		drawAtTile(player.character.sprite, 8, 0, x, y)
 
-	drawAtTile(tiles.ladder.sprite, 2, 4, x, y)
-	drawAtTile(animalList.pitbull.sprite, 3, 4, x, y)
-	drawAtTile(player.character.sprite, 0, 4, x, y)
+		drawAtTile(tiles.ladder.sprite, 2, 4, x, y)
+		drawAtTile(animalList.pitbull.sprite, 3, 4, x, y)
+		drawAtTile(player.character.sprite, 0, 4, x, y)
 
-	drawAtTile(tiles.ladder.sprite, 9, 4, x, y)
-	drawAtTile(animalList.pitbull.sprite, 10, 4, x, y)
-	drawAtTile(player.character.sprite, 9, 4, x, y)
-end
+		drawAtTile(tiles.ladder.sprite, 9, 4, x, y)
+		drawAtTile(animalList.pitbull.sprite, 10, 4, x, y)
+		drawAtTile(player.character.sprite, 9, 4, x, y)
+	end
 }
+
 P.wireCutterScreens = {}
 P.waterScreens = {}
 P.spongeScreens = {}
@@ -96,7 +109,7 @@ P.screenLevel = 1
 function P.draw()
 	local screen = P.screens[P.currentScreen][P.screenLevel]
 	drawHere(P.mainScreen, 0, 0)
-	screen(30, 64, scale)
+	screen(30, 70, scale)
 	drawHere(P.manuelTint, 40*P.currentScreen-18, 5)
 	for i = 1, #(P.screens[P.currentScreen]) do
 		if P.screenLevel == i then
