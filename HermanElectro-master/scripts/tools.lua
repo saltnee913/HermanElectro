f543714dc70553cebc5f70fd07b34ded42488e80
eknighton <eknighton@me.com>
2017-05-10 12:04:20 -0700

    Made superwirecutter desc same as knife

--- HermanElectro-master/scripts/tools.lua
+++ HermanElectro-master/scripts/tools.lua
@@ -1434,9 +1434,9 @@ function P.boomboxSpawner:useToolNothing(tileY, tileX)
 	pushables[#pushables+1] = toSpawn
 end
 
-P.superWireCutters = P.wireCutters:new{name = "Super Wire-cutters", description = "A cut above", -- Anything more ... you know
+P.superWireCutters = P.wireCutters:new{name = "Super Wire-cutters", description = "They can't stop you", -- Anything more ... you know
 image = 'Graphics/wirecutters.png', quality = -1}-- was "Super Wire Cutters" ... Stone Splitters sounds cool but misleading.. Arkham's razor, Laser razor, Incsors: Something to chew on
-function P.superWireCutters:usableOnNonOverlay(tile)
+function P.superWireCutters:usableOnNonOverlay(tile) -- "A cut above"
 	return not tile.destroyed and (tile:instanceof(tiles.wire)
 	or tile:instanceof(tiles.conductiveGlass) or tile:instanceof(tiles.reinforcedConductiveGlass) or tile:instanceof(tiles.electricFloor))
 end
